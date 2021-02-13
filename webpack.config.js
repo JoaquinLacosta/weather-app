372f2ad2df26afb64cad3ba97e76ef5591cb3558
Joaquin Lacosta <joacolacosta2001@gmail.com>
2021-02-13 19:23:41 -0300

    configuracion inicial de webpack y babel

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    mode: "development",
+    devServer: {
+        open: true,
+        port: 8000,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {   test: /\.(js|jsx)$/, 
+                exclude: /(node_modules|bower_components)/, 
+                use: { 
+                    loader: 'babel-loader',
+                }},
+            {
+                test: /\.(s*)css$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                  },
+                  'css-loader',
+                  'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: 'html-loader'
+                  }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/public/index.html",
+            filename: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+}
